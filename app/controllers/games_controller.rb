3787789921e3b36b5e0555ffd1c92d28f64debb9
David Gregorato <hydrosnake@gmail.com>
2021-08-09 17:03:13 +0900

    Connected the API, need to work on the validation process.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,8 @@
-class GamesController < ApplicationController
+require 'json'
+require 'open-uri'
 
+# dsaghadksghfjkds
+class GamesController < ApplicationController
   def new
     alphabet = ('a'..'z').to_a
     @letters = []
@@ -7,7 +10,32 @@ class GamesController < ApplicationController
   end
 
   def score
-    params[:attempt]
-    raise
+    try = params[:attempt].downcase
+    # raise
+    letters = params[:letters].downcase
+
+    @result = if letters_are_valid?(letters, try) && validate_word?(try)
+                "Congratulations, #{try} is valid and is in the dictionary!"
+              elsif letters_are_valid?(letters, try)
+                "#{try} uses all the letters given to you but we couldn't find it in our dictionary."
+              elsif validate_word?(try)
+                "#{try} is in our dictionary but it contains letters not present in the original letters we gave you."
+              else 'Not your best effort.'
+              end
+  end
+
+  private
+
+  def letters_are_valid?(letters, try)
+    letters.chars.all? do |letter|
+      try.count(letter) <= letters.count(letter)
+    end
+  end
+
+  def validate_word?(try)
+    url = "https://wagon-dictionary.herokuapp.com/#{try}"
+    page = URI.open(url).read
+    word = JSON.parse(page)
+    word['found']
   end
 end
